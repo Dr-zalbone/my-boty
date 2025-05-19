1a0cc2055d711ced03127e3b4acc133987111f47
Dr-zalbone <dr.zalbone@gmail.com>
2025-05-19 10:50:13 +0300

    its broken...

--- sketch_may19a/sketch_may19a.ino
+++ sketch_may19a/sketch_may19a.ino
@@ -1,10 +1,18 @@
 void setup() {
-  /hi
-
+  Serial.begin(9600);
 }
 
 void loop() {
-  // hello
-
+  Serial.print(analogRead(A0));
+  Serial.print(" ");
+  Serial.print(analogRead(A1));
+  Serial.print(" ");
+  Serial.print(analogRead(A2));
+  Serial.print(" ");
+  Serial.print(analogRead(A3));
+  Serial.print(" ");
+  Serial.print(analogRead(A4));
+  Serial.print(" ");
+  Serial.println(analogRead(A5));
 
 }
